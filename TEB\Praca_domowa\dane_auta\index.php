0caba22f2307f7d0fac7eaee7df178e2b63052fb
Pajor2000 <92211464+Pajor2000@users.noreply.github.com>
2021-10-17 15:32:28 +0200

    Create TEB\Praca_domowa\dane_auta\index.php

---
+++ TEB\Praca_domowa\dane_auta\index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Wynik</title>
+  </head>
+  <body>
+
+    <?php
+    if () {
+      $_POST['mark']
+
+    }
+
+
+     ?>
+
+  </body>
+</html>
