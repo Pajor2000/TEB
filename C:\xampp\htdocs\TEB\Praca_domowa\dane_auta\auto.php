e0e4ccc4e5709dfbd5c988cd276b70ec15799717
Pajor2000 <92211464+Pajor2000@users.noreply.github.com>
2021-10-17 15:28:12 +0200

    Update C:\xampp\htdocs\TEB\Praca_domowa\dane_auta\auto.php

--- C:\xampp\htdocs\TEB\Praca_domowa\dane_auta\auto.php
+++ C:\xampp\htdocs\TEB\Praca_domowa\dane_auta\auto.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dane auta</title>
+  </head>
+  <body>
 
+    <form action="index.php" method="post">
+      <input type="text" name="mark" placeholder="marka"><br><br>
+      <input type="text" name="model" placeholder="model"><br><br>
+      <input type="text" name="color" placeholder="color"><br><br>
+      <input type="text" name="year" placeholder="rok"><br><br>
+    <a href="index.php">  <input type="button" name="button" value="Wyślij"></a>
+    </form>
+
+  </body>
+</html>
