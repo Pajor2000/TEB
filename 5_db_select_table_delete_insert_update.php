e15b36ea32dbd42e63423173673d7b743edc1b26
Pajor2000 <92211464+Pajor2000@users.noreply.github.com>
2021-11-06 16:20:49 +0100

    Add files via upload

--- 5_db_select_table_delete_insert_update.php
+++ 5_db_select_table_delete_insert_update.php
@@ -41,13 +41,15 @@ TABLE;
             <td>$row[name]</td>
             <td>$row[surname]</td>
             <td>$row[city]</td>
-            <td>$row[birthday]</td><td><a href="./scripts/delete.php?deleteId=$row[user_id]">Usuń</a></td>
-            <td><a href="./scripts/update.php?updateId=$row[user_id]">Aktualizuj</a></td>
+            <td>$row[birthday]</td>
+            <td><a href="./scripts/delete.php?deleteId=$row[user_id]">Usuń</a></td>
+            <td><a href="./5_db_select_table_delete_insert_update.php?updateId=$row[user_id]">Aktualizuj</a></td>
           <tr>
 USER;
       }
       echo '</table>';
-//dodawanie użytkownika
+
+// dodawanie użytkownika
       if (isset($_GET['addUser'])) {
         if (isset($_SESSION['error'])) {
           echo $_SESSION['error'];
@@ -55,75 +57,69 @@ USER;
         }
         echo <<< FORMADDUSER
           <hr>
-          <h4>Aktualizacja użytkownika</h4>
+          <h4>Dodawanie użytkownika</h4>
           <form action="./scripts/insert.php" method="post">
             <input type="text" name="name" placeholder="Podaj imię"><br><br>
             <input type="text" name="surname" placeholder="Podaj nazwisko"><br><br>
             <select name="city_id">
 FORMADDUSER;
-  $sql="SELECT * FROM `cities`";
-  $result=$connect->query($sql);
-  while ($city=$result->fetch_assoc()) {
-    echo <<< CITY
-      <option value="$city[city_id]">$city[city]</option>
+              $sql="SELECT * FROM `cities` ";
+              $result=$connect->query($sql);
+              while ($city=$result->fetch_assoc()) {
+                echo <<< CITY
+                  <option value="$city[city_id]">$city[city]</option>
 CITY;
-  }
-
-
-echo <<< FORMADDUSER
-</select><br><br>
+              }
+        echo <<< FORMADDUSER
+            </select><br><br>
             <input type="date" name="birthday"> Data urodzenia<br><br>
             <input type="submit" value="Dodaj użytkownika">
           </form>
 FORMADDUSER;
-
       }else {
-        echo '<hr><a href="./4_db_select_table_delete_insert.php?addUser=">Dodaj użytkownika</a>';
+        echo '<hr><a href="./5_db_select_table_delete_insert_update.php?addUser=">Dodaj użytkownika</a>';
       }
 
-//aktualizacja użytkownika
-
+// aktualizacja użytkownika
 if (isset($_GET['updateId'])) {
   if (isset($_SESSION['error'])) {
     echo $_SESSION['error'];
     unset($_SESSION['error']);
   }
 
-$sql ="SELECT * FROM `users` WHERE `user_id`=$_GET[updateId];";
+$sql = "SELECT * FROM `users` WHERE `user_id`=$_GET[updateId];";
 $result = $connect->query($sql);
 $userUpdate = $result->fetch_assoc();
 
   echo <<< FORMADDUSER
     <hr>
-    <h4>Dodawanie użytkownika</h4>
-    <form action="./scripts/insert.php" method="post">
-      <input type="text" name="name" value=$userUpdate[name] placeholder="Podaj imię"><br><br>
-      <input type="text" name="surname" value=$userUpdate[surname] placeholder="Podaj nazwisko"><br><br>
+    <h4>Aktualizacja użytkownika</h4>
+    <form action="./scripts/update.php" method="post">
+      <input type="hidden" name="user_id" value="$userUpdate[user_id]"><br><br>
+      <input type="text" name="name" value="$userUpdate[name]"><br><br>
+      <input type="text" name="surname" value="$userUpdate[surname]"><br><br>
       <select name="city_id">
 FORMADDUSER;
-$sql="SELECT * FROM `cities`";
-$result=$connect->query($sql);
-while ($city=$result->fetch_assoc()) {
-  if ($userUpdate['city_id'] == $city['city_id']) {
-
-    echo <<< CITY
-    <option value="$city[city_id]" selected>$city[city]</option>
-    CITY;
-
-  }
-echo <<< CITY
-<option value="$city[city_id]">$city[city]</option>
+        $sql="SELECT * FROM `cities` ";
+        $result=$connect->query($sql);
+        while ($city=$result->fetch_assoc()) {
+          if ($userUpdate['city_id'] == $city['city_id']) {
+            echo <<< CITY
+              <option value="$city[city_id]" selected>$city[city]</option>
+CITY;
+          }else{
+            echo <<< CITY
+              <option value="$city[city_id]">$city[city]</option>
 CITY;
 }
 
-
-echo <<< FORMADDUSER
-</select><br><br>
-      <input type="date" name="birthday" value=$userUpdate[birthday]> Data urodzenia<br><br>
-      <input type="submit" value="Dodaj użytkownika">
+        }
+  echo <<< FORMADDUSER
+      </select><br><br>
+      <input type="date" name="birthday" value="$userUpdate[birthday]"> Data urodzenia<br><br>
+      <input type="submit" value="Aktualizacja użytkownika">
     </form>
 FORMADDUSER;
-
 }
      ?>
 
